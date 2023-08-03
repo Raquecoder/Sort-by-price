f95220fd41c5d796e3f130b2260df8e1728f80dc
Raquel Campos <112841246+Raquecoder@users.noreply.github.com>
2023-08-03 15:33:06 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const sortBtn = document.getElementById('sort-btn')
+let container=document.getElementById('container')
+            
+sortBtn.addEventListener('click', function(){
+container.classList.toggle('reverse')
+/*
+Challenge:
+1. Make the Sort by Price button 
+   reverse the order of the paintings.
+*/
+
+})
